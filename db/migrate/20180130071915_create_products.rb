4217332037b4f2cfe35a6499f5ece504710c289d
Sean-Paul Gómez <32402992+FakeOn3@users.noreply.github.com>
2018-01-30 02:43:46 -0500

    Primer Commit
    CRUD y modelo de Product.

---
+++ db/migrate/20180130071915_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.decimal :price
+      t.integer :stock
+      t.boolean :status
+
+      t.timestamps
+    end
+  end
+end
